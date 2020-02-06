eeb68ecdfb60a50ff91b4548af345561d6dc5d1b
Robert Perillo <perillo.robert@gmail.com>
2020-02-06 10:30:54 -0700

    remove commented lines

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-// $('#text-input').value);
 
-// $('#submit-button').on('click', function(e){
-//     e.preventDefault();
-//     console.log($('form').method)
-//     let $task = $( '<tr> <td> <input type="checkbox" class="checkbox">yoasdads</td> </tr>' );
-//     $('table').append($task);
-// })
 $('#submit-button').on('click', function(){
     let $text = $('#text-input').val();
     let $task = $( `<tr> <td> <button class="btn btn-danger">X</button>&nbsp&nbsp${$text}</td> </tr>` );
@@ -16,7 +9,6 @@ $('#submit-button').on('click', function(){
 
 
 $('.table').on('click', '.btn-danger', function(){
-    // $(this).text = 'X';
     $(this).closest('td').fadeOut();
 })
 
